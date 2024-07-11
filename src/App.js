f6d7acb8fbc5419bb91a26fb5cee71de3f47d09a
keedpek <zaharv20004@gmail.com>
2024-07-11 22:34:28 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-const App = () => (
-  <h1>Init</h1>
-);
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './utils/Router/AppRouter';
+import Header from './components/Header/index.jsx';
+import Footer from './components/Footer/index.jsx';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <AppRouter />
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
